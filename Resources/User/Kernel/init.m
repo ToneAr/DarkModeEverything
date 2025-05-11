cec03933d8713b0dc542e04ea0f11dbb9bdda8e1
tonya <tonya@wolfram.com>
2025-05-11 03:02:13 +0100

    Refactor Find Notebook and Update init.m
    - Removed internal cache information and unnecessary comments from Find.nb to streamline the notebook content.
    - Updated init.m to suppress warnings during Paclet installation by using Quiet, enhancing the initialization process.
    - Removed unused helper functions related to building and deploying paclets from init.m for cleaner code.

--- Resources/User/Kernel/init.m
+++ Resources/User/Kernel/init.m
@@ -4,7 +4,7 @@
 If[$Notebooks,
 
 	(* ::Section:: *)(* Initialization *)
-	PacletInstall[CloudObject["user:tonya/PacletSites/DME/DME"]];
+	Quiet @ PacletInstall[CloudObject["user:tonya/PacletSites/DME/DME"]];
 	<<TonyAristeidou`DME`;
 
 	(* ::Section:: *)(* Sugar *)
@@ -24,26 +24,5 @@ If[$Notebooks,
 (* ::Section:: *)(* Kernel tweaks *)
 $HistoryLength = 100;
 
-(* ::Section:: *)(* Helper functions *)
-Options[buildAndDeployPaclet]={
-	Permissions -> "Public"
-};
-buildAndDeployPaclet[dir_, cloudLoc_, OptionsPattern[]] := Module[{
-		res
-	},
-	Needs["PacletTools`"];
-	res = PacletTools`PacletBuild[ dir ];
-	Quiet[
-		DeleteObject[CloudObject[cloudLoc]],
-		CloudObject::cloudnf
-	];
-	CopyFile[
-		res["PacletArchive"],
-		CloudObject[cloudLoc],
-		OverwriteTarget->True
-	];
-	SetPermissions[CloudObject[cloudLoc], OptionValue[Permissions]];
-	CloudObject[cloudLoc]
-];
 (* :!CodeAnalysis::EndBlock:: *)
 (*!* end:DME *!*)
